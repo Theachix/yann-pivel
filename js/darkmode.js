f6c144b840f0b580f6b839ff65fb49419963ea7a
Theachix <83505344+Theachix@users.noreply.github.com>
2021-07-01 16:46:24 +0200

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -6,8 +6,11 @@ Switch.addEventListener('click', () => {
     if(body.classList.contains('dark')){
         body.classList.add('light')
         body.classList.remove('dark')
+        Switch.innerHTML = '<i class="bi bi-moon"></i>'
+
     } else if (body.classList.contains('light')){
         body.classList.add('dark')
         body.classList.remove('light')
+        Switch.innerHTML = '<i class="bi bi-sun"></i>'
     }
 })
