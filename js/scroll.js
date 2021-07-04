3d3391b58de9e963a9038e29e238f6a46cf349e6
Theachix <83505344+Theachix@users.noreply.github.com>
2021-07-04 18:17:08 +0200

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+// const sr = ScrollReveal();
+// let title = document.getElementsByClassName('section-heading');
+
+// sr.reveal('title', {
+
+
+// })
+
+
+// const sr = ScrollReveal();
+
+ScrollReveal({reset:true});
+ScrollReveal().reveal('.section-heading')
+
+
+
+ScrollReveal().reveal('.me');
+ScrollReveal().reveal('.devp', { delay: 500 });
+ScrollReveal().reveal('.format', { delay: 1500 });
