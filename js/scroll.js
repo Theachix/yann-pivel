f055a12db395b60c527bdcbcc827e1cf6379a49c
Theachix <83505344+Theachix@users.noreply.github.com>
2021-07-04 18:26:33 +0200

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,3 @@
-// const sr = ScrollReveal();
-// let title = document.getElementsByClassName('section-heading');
-
-// sr.reveal('title', {
-
-
-// })
-
-
-// const sr = ScrollReveal();
 
 ScrollReveal({reset:true});
 ScrollReveal().reveal('.section-heading')
@@ -17,3 +7,8 @@ ScrollReveal().reveal('.section-heading')
 ScrollReveal().reveal('.me');
 ScrollReveal().reveal('.devp', { delay: 500 });
 ScrollReveal().reveal('.format', { delay: 1500 });
+
+
+ScrollReveal().reveal('.dates', { delay: 300 });
+ScrollReveal().reveal('.subheading', { delay: 400 });
+ScrollReveal().reveal('.details', { delay: 500 });
